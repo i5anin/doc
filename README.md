3dd0a60d050b394ba22525224fd708dd7b643896
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-07-17 11:00:10 +0300

    -pages

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-## Содержание Pages
-
-- [algoritm](https://i5anin.github.io/doc/algoritm/)
-
-- [sql](https://i5anin.github.io/doc/sql/)
-
 ## Содержание Markdown
 
 - [algoritm](algoritm.md)
--- js/main.js
+++
@@ -1,8 +0,0 @@
-function loadMarkdown(file) {
-  fetch(file)
-    .then((response) => response.text())
-    .then((markdown) => {
-      const html = marked(markdown);
-      document.getElementById("markdown-container").innerHTML = html;
-    });
-}
